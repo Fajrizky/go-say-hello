8bbf41dba351968133c9b5976347bc42b4c7e6f2
Fajrizky <muhammadfajrizky@gmail.com>
2021-09-19 10:23:54 +0700

    update say hello with param

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Fajrizky/go-say-hello
+module github.com/Fajrizky/go-say-hello/v2
 
 go 1.16
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello Fajrizky"
+func SayHello(lastName string) string {
+	return "Hello Fajrizky" + lastName
 }
